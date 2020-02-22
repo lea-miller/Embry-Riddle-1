a44fa6fd85bd4e3e955fc616b922ceaac305454c
Michael Fornito <fornitomichael@gmail.com>
2020-02-22 15:03:00 -0500

    Revert "Revert "Setup the suit bars""
    This reverts commit 1e5d8938d275b1eb29fdddd009693ff9e5743665.

---
+++ ALEXEI_MAIN/Assets/MetricScripts/SuitMetricBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SuitMetricBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image metricStatusBar;
+
+    public void setMaxMetricValue(int metricStatus)
+    {
+       slider.maxValue = metricStatus;
+       slider.value = metricStatus;
+       metricStatusBar.color =  gradient.Evaluate(1f);
+    }
+
+    public void setMetricValue(int metricStatus)
+    {
+        slider.value = metricStatus;
+        metricStatusBar.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+}
