568fd7e66e30c05053fa06e3f0d24b5171c5de9f
Michael Fornito <fornitomichael@gmail.com>
2020-02-22 14:56:35 -0500

    Setup the suit bars
    When navigating to scene two the O2 bar and battery bar are activated.
    
    There is a generic script called MetricBar that shall be used for every suits status bar to avoid duplication

---
+++ ALEXEI_MAIN/Assets/MetricScripts/MetricsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetricsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int maxOxygen , currentOxygen, maxBattery, currentBattery;
+    
+    public SuitMetricBar oxygenStatusBar, batteryStatusBar;
+    
+    void Start()
+    {
+        currentOxygen = currentBattery = maxOxygen;
+        batteryStatusBar.setMaxMetricValue(maxBattery);
+        oxygenStatusBar.setMaxMetricValue(maxOxygen);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentOxygen = currentOxygen - 20;
+        currentBattery = currentBattery - 10;
+        
+        batteryStatusBar.setMetricValue(currentBattery);
+        oxygenStatusBar.setMetricValue(currentOxygen);
+    }
+}
