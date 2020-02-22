1e5d8938d275b1eb29fdddd009693ff9e5743665
Michael Fornito <fornitomichael@gmail.com>
2020-02-22 14:57:49 -0500

    Revert "Setup the suit bars"
    This reverts commit 568fd7e66e30c05053fa06e3f0d24b5171c5de9f.

---
+++ ALEXEI_MAIN/Assets/VitalsInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VitalsInput : MonoBehaviour
+{
+    private float O2 = 500f;
+
+    public float getOxygen()
+    {
+        return O2;
+    }
+}
