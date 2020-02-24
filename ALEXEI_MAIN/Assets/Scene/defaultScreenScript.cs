b02ec988546e36865325d0367425e16f6d89e41b
Michael Fornito <fornitomichael@gmail.com>
2020-02-23 23:14:04 -0500

    default screen script
    Created a default screen script to activate and deactive the specfic screens on startup regardless what is active or deactive on the scene editor within Unity

--- ALEXEI_MAIN/Assets/Scene/defaultScreenScript.cs
+++ ALEXEI_MAIN/Assets/Scene/defaultScreenScript.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class defaultScreenScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject homeScreen, vitalScreen, noteScreen, taskScreen;
     void Start()
     {
-        
+        homeScreen.SetActive(true);
+        vitalScreen.SetActive(false);
+        noteScreen.SetActive(false);
+        taskScreen.SetActive(false);
     }
 }
--- ALEXEI_MAIN/Assets/voiceRecognition/voiceCommand.cs
+++ ALEXEI_MAIN/Assets/voiceRecognition/voiceCommand.cs
@@ -17,7 +17,6 @@ public class voiceCommand : MonoBehaviour
     void Start()
     {
 
-        vitalScreen.SetActive(false);
         initalizeDictionary();
         initalizeSpeechCommand();
     }
