51b0dbe21635bf6b342b45970c4d851225c6ab54
Michael Fornito <fornitomichael@gmail.com>
2020-03-04 11:26:16 -0500

    Voice Recognition Updated
    hasn't been tested yet for bugs, will test before pushing to master

--- ALEXEI_WithMagicLeap/Assets/voiceRecognition/voiceCommand.cs
+++ ALEXEI_WithMagicLeap/Assets/voiceRecognition/voiceCommand.cs
@@ -12,7 +12,7 @@ public class voiceCommand : MonoBehaviour
     private KeywordRecognizer mainTriggerRecognizer;
     private Dictionary<string, System.Action> mainTrigger;
     private Dictionary<string, System.Action> keywords;
-    public GameObject vitalScreen, homeScreen;
+    public GameObject vitalScreen, homeScreen, noteScreen, taskScreen;
 
     void Start()
     {
@@ -23,21 +23,37 @@ public class voiceCommand : MonoBehaviour
 
     private void initalizeDictionary()
     {
-            mainTrigger = new Dictionary<string, System.Action>();
-            mainTrigger.Add("ALEXEI start up", startUp);
-            mainTrigger.Add("ALEXEI over and out", closeDown);
+        mainTrigger = new Dictionary<string, System.Action>();
+        mainTrigger.Add("ALEXEI start up", startUp);
+        mainTrigger.Add("ALEXEI over and out", closeDown);
+        mainTrigger.Add("ALEXEI display suit vitals", changeToVitalsScreen);
+        mainTrigger.Add("ALEXEI display home screen", changeToMainScreen);
+        mainTrigger.Add("ALEXEI display notes screen", changeToNoteScreen);
+        mainTrigger.Add("ALEXEI display tasks screen", changeToTaskScreen);
+        mainTrigger.Add("ALEXEI show home screen", changeToMainScreen);
+        mainTrigger.Add("ALEXEI show suit vitals screen", changeToVitalsScreen);
+        mainTrigger.Add("ALEXEI show notes screen", changeToNoteScreen);
+        mainTrigger.Add("ALEXEI show tasks screen", changeToTaskScreen);
+        mainTrigger.Add("ALEXEI take me home", changeToMainScreen);
 
-            keywords = new Dictionary<string, System.Action>();
-            keywords.Add("Display suit vitals", changeSuitVitalsScene);
-            keywords.Add("Dispaly main screen", changeToMainScene);
+        keywords = new Dictionary<string, System.Action>();
+        keywords.Add("Display suit vitals", changeToVitalsScreen);
+        keywords.Add("Dispaly main screen", changeToMainScreen);
+        keywords.Add("Display notes screen", changeToNoteScreen);
+        keywords.Add("Display tasks screen", changeToTaskScreen);
+        keywords.Add("Show suit vitals", changeToVitalsScreen);
+        keywords.Add("Show main screen", changeToMainScreen);
+        keywords.Add("Show notes screen", changeToNoteScreen);
+        keywords.Add("Show tasks screen", changeToTaskScreen);
+        keywords.Add("Take me home", changeToMainScreen);
     }
 
     private void initalizeSpeechCommand()
     {
 
-            mainTriggerRecognizer = new KeywordRecognizer(mainTrigger.Keys.ToArray());
-            mainTriggerRecognizer.OnPhraseRecognized += TriggerRecognizedSpeech;
-            mainTriggerRecognizer.Start();
+        mainTriggerRecognizer = new KeywordRecognizer(mainTrigger.Keys.ToArray());
+        mainTriggerRecognizer.OnPhraseRecognized += TriggerRecognizedSpeech;
+        mainTriggerRecognizer.Start();
     }
 
     private void startUp()
@@ -46,8 +62,8 @@ public class voiceCommand : MonoBehaviour
         {
             keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
             keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
-            keywordRecognizer.Start();
         }
+        keywordRecognizer.Start();
     }
 
     private void TriggerRecognizedSpeech(PhraseRecognizedEventArgs speech)
@@ -63,21 +79,37 @@ public class voiceCommand : MonoBehaviour
         keywords[speech.text].Invoke();
     }
 
-    private void changeSuitVitalsScene()
-    {
-        vitalScreen.SetActive(true);
-        homeScreen.SetActive(false);
-    }
-
     private void closeDown()
     {
         keywordRecognizer.Stop();
     }
 
-    private void changeToMainScene()
+    private void changeToVitalsScreen()
     {
-       homeScreen.SetActive(true);
-       vitalScreen.SetActive(false);
+        changeScene(false, true, false, false);
+    }
+
+    private void changeToNoteScreen()
+    {
+        changeScene(false, false, true, false);
+    }
+
+    private void changeToTaskScreen()
+    {
+        changeScene(false, false, false, true);
+    }
+
+    private void changeToMainScreen()
+    {
+        changeScene(true, false, false, false);
+    }
+
+    private void changeScene(Boolean isHomeScreenActive, Boolean isVitalScreenActive, Boolean isNoteScreenActive, Boolean isTaskScreenActive)
+    {
+        homeScreen.SetActive(isHomeScreenActive);
+        vitalScreen.SetActive(isVitalScreenActive);
+        noteScreen.SetActive(isNoteScreenActive);
+        taskScreen.SetActive(isTaskScreenActive);
     }
 }
 
