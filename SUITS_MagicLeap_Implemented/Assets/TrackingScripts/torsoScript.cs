b362445c58d2fbf5082bb6a620c495b35ecedb73
Michael Fornito <fornitomichael@gmail.com>
2020-03-25 23:41:30 -0400

    The new file for Magic Leap!

---
+++ SUITS_MagicLeap_Implemented/Assets/TrackingScripts/torsoScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torsoScript : MonoBehaviour
+{
+
+    public Camera mainCamera;
+    private Vector3 cameraPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraPos.x = mainCamera.transform.position.x;
+        cameraPos.z = mainCamera.transform.position.z;
+        cameraPos.y = mainCamera.transform.position.y;
+        transform.position = cameraPos;
+    }
+}
