641c68125365097223c1ba4e19b35b5c9b441016
Michael Fornito <fornitomichael@gmail.com>
2020-04-09 04:46:18 -0400

    Audio Recording
    This push does not actually save the audio yet even though it has been implemented. That functionaliy will come in another commit, currently working on improving export performance time

--- SUITS_MagicLeap_Implemented/Assets/TrackingScripts/trackCanvasScreen.cs
+++ SUITS_MagicLeap_Implemented/Assets/TrackingScripts/trackCanvasScreen.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class trackCanvasScreen : MonoBehaviour
 {
     public GameObject currentScreen;
-    private float rotation = 0.4f;
+   // private float rotation = 0.4f;
     private Quaternion currentScreenRotation, torsoRot;
     private GameObject torso, controller;
     void Start()
@@ -25,13 +25,12 @@ public class trackCanvasScreen : MonoBehaviour
     //The roation of the camera causes the current canvas to rotate without changing its position
     void rotationSync()
     {
-        Vector3 currentScreenForward = Camera.main.transform.forward * rotation;
+        Vector3 currentScreenForward = Camera.main.transform.forward;
         currentScreen.transform.forward = currentScreenForward;
     }
 
     /*The pointer of the remote is in the z axis by the MagicLeap Devs
     Therefore to have the remote vertical to mount on torso it must be in the y plane
-    Since the touchpad of the remote will be on the chest, the direction is in the -y plane
     */
     public void updateTorsoWithRemote()
     {
@@ -39,9 +38,10 @@ public class trackCanvasScreen : MonoBehaviour
         torso.transform.rotation = torsoRot;
     }
 
+    // Since the touchpad of the remote will be facing the chest, the direction facing away from the chest is in the -y plane
     private void distanceFromHead()
     {
-        Vector3 canvasDistance = new Vector3(transform.position.x, -50f/100, transform.position.z);
+        Vector3 canvasDistance = new Vector3(transform.position.x, -20f/100, transform.position.z);
         transform.position = canvasDistance;
     }
 
