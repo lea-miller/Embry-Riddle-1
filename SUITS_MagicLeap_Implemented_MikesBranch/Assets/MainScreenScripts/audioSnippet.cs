7b0083c02ed37eb03b5e2566d41ac31a37c54360
Michael Fornito <fornitomichael@gmail.com>
2020-04-10 14:35:17 -0400

    I deleted everything!

---
+++ SUITS_MagicLeap_Implemented_MikesBranch/Assets/MainScreenScripts/audioSnippet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.MagicLeap;
+
+public class audioSnippet :  MonoBehaviour
+{
+    private AudioSource source;
+    private recordCounter recordCounter;
+	
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        recordCounter = GetComponent<recordCounter>();
+
+		if (Microphone.devices.Length <= 0)
+        {
+            //Throw a warning message at the console if there isn't  
+            Debug.LogWarning("Microphone not connected!");
+        }
+    }
+
+    public void recordStartAudio()
+    {
+            source.Stop();
+            source.clip = Microphone.Start("", false, recordCounter.getMaxRecordingTime(), 44100);
+    }
+
+    public void recordAudioFinished()
+    {
+            Microphone.End("");
+    }
+
+    public void recordSaveAudio()
+    {
+            SavWav.Save("audio1", source.clip);           
+    }
+
+    public void recordPlayAudio()
+    {
+            Microphone.End("");
+            source.Play();
+    }
+}
---
+++ SUITS_MagicLeap_Implemented_MikesBranch/Assets/TrackingScripts/trackCanvasScreen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackCanvasScreen : MonoBehaviour
+{
+    public GameObject currentScreen;
+   // private float rotation = 0.4f;
+    private Quaternion currentScreenRotation, torsoRot;
+    private GameObject torso, controller;
+    void Start()
+    {
+        controller = GameObject.FindWithTag("MLController");
+        torso = GameObject.FindWithTag("Torso");
+        distanceFromHead();
+    }
+       
+    // Update is called once per frame
+    void Update()
+    {
+        rotationSync();
+        updateTorsoWithRemote();
+    }
+
+    //The roation of the camera causes the current canvas to rotate without changing its position
+    void rotationSync()
+    {
+        Vector3 currentScreenForward = Camera.main.transform.forward;
+        currentScreen.transform.forward = currentScreenForward;
+    }
+
+    /*The pointer of the remote is in the z axis by the MagicLeap Devs
+    Therefore to have the remote vertical to mount on torso it must be in the y plane
+    */
+    public void updateTorsoWithRemote()
+    {
+        torsoRot = controller.transform.rotation;
+        torso.transform.rotation = torsoRot;
+    }
+
+    // Since the touchpad of the remote will be facing the chest, the direction facing away from the chest is in the -y plane
+    private void distanceFromHead()
+    {
+        Vector3 canvasDistance = new Vector3(transform.position.x, -20f/100, transform.position.z);
+        transform.position = canvasDistance;
+    }
+
+}
