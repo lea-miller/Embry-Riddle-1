7eeacf68753fc60209236345172581f82442d220
Michael Fornito <fornitomichael@gmail.com>
2020-04-18 23:14:03 -0400

    Addded Files

---
+++ SUITS_MagicLeap_Implemented_/Assets/MainScreenScripts/audioSnippet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.MagicLeap;
+
+public class audioSnippet :  MonoBehaviour
+{
+    private AudioSource source;
+    private recordCounter recordCounter;
+	
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        recordCounter = GetComponent<recordCounter>();
+
+		if (Microphone.devices.Length <= 0)
+        {
+            //Throw a warning message at the console if there isn't  
+            Debug.LogWarning("Microphone not connected!");
+        }
+    }
+
+    public void recordStartAudio()
+    {
+            source.Stop();
+            source.clip = Microphone.Start("", false, recordCounter.getMaxRecordingTime(), 44100);
+    }
+
+    public void recordAudioFinished()
+    {
+            Microphone.End("");
+    }
+
+    //TODO: Will have to wrap into a prossible thread
+    public void recordSaveAudio()
+    {
+            SavWav.Save("audio1", source.clip);           
+    }
+
+    public void recordPlayAudio()
+    {
+            Microphone.End("");
+            source.Play();
+    }
+}
---
+++ SUITS_MagicLeap_Implemented_/Assets/VitalScripts/SuitVitalBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SuitVitalBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image vitalStatusBar;
+
+    public void setMaxVitalsValue(int vitalStatus)
+    {
+       slider.maxValue = vitalStatus;
+       slider.value = vitalStatus;
+       vitalStatusBar.color =  gradient.Evaluate(1f);
+    }
+
+    public void setVitalsValue(int vitalStatus)
+    {
+        slider.value = vitalStatus;
+        vitalStatusBar.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+}
