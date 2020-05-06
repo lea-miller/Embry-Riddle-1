871aa1f7ab2154d4334c6de6ea3b908ac2fdeb44
RWFlanagan <60719044+RWFlanagan@users.noreply.github.com>
2020-05-06 12:48:52 -0400

    BPM and Temp from TextMeshPro

--- SUITS_MagicLeap_Implemented_/Assets/VitalScripts/VitalsManager.cs
+++ SUITS_MagicLeap_Implemented_/Assets/VitalScripts/VitalsManager.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class VitalsManager : MonoBehaviour
 {
      private int maxPO2 = 500000;
      private int maxBattery = 30000;
-    [SerializeField] private int  currentPO2, currentBattery, currentPsi, currentHeartRate, currentCo2;
-    private GameObject PO2VitalDispNum, batteryVitalDispNum;
+    [SerializeField] private int currentPO2, currentBattery, currentBPM, currentTSUB;
+    private GameObject PO2VitalDispNum, batteryVitalDispNum, BPMDispNum, TSUBDispNum;
     [SerializeField] private SuitVitalBar batteryStatusBar, PO2StatusBar;
     [SerializeField] private MainScreenVitalManager homeScreen;
 
@@ -24,6 +25,8 @@ public class VitalsManager : MonoBehaviour
         currentBattery = maxBattery;
         PO2VitalDispNum = GameObject.Find("/Torso/VitalsScreen/PO2BarBody/PO2BarSlider/PO2NumericalValue");
         batteryVitalDispNum = GameObject.Find("/Torso/VitalsScreen/BatteryBarBody/BatteryBarSlider/BatteryNumericalValue");
+        BPMDispNum = GameObject.Find("/Torso/VitalsScreen/BPMValue");
+        TSUBDispNum = GameObject.Find("/Torso/VitalsScreen/TSUBValue");
     }
 
     // Update is called once per frame
@@ -31,9 +34,8 @@ public class VitalsManager : MonoBehaviour
     {
         changeBattery();
         changePO2();
-        changeHeartRate();
-        changeCo2();
-        changePsi();
+        changeBPM();
+        changeTSUB();
         updateVitalScreen();
         //homeScreen.updateMainScreen(currentPO2,currentCo2,currentPsi,currentHeartRate);
     }
@@ -46,29 +48,25 @@ public class VitalsManager : MonoBehaviour
 
     private void changePO2()
     {
-        currentPO2 = currentPO2 - 1000;
+        currentPO2 = currentPO2 - 10;
         PO2StatusBar.setVitalsValue(currentPO2);
     }
 
-    private void changeHeartRate()
+    private void changeBPM()
     {
-        currentHeartRate = Random.Range(60, 65);
+        currentBPM = 60;
     }
 
-    private void changeCo2()
+    private void changeTSUB()
     {
-        currentCo2 = currentCo2 + 20;
+        currentTSUB = 4;
     }
-
-    private void changePsi()
-    {
-        currentPsi = 5;
-    }
-
     private void updateVitalScreen()
     {
         PO2VitalDispNum.GetComponent<Text>().text = currentPO2.ToString() + " psia";
         batteryVitalDispNum.GetComponent<Text>().text = currentBattery.ToString() + " Ah";
+        BPMDispNum.GetComponent<TextMeshProUGUI>().text = currentBPM.ToString() + " BPM";
+        TSUBDispNum.GetComponent<TextMeshProUGUI>().text = currentTSUB.ToString() + " degF";
     }
 
 }
