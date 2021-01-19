83f1db3b043d9aee4252dc82afdd6357e91fa5c7
lopacn <60628533+lopacn@users.noreply.github.com>
2021-01-19 17:28:24 -0500

    Nick's Normcore Voice and Exerimentation
    First go at adding voice, sucessful build to magic leap. Experimenting with layout of new Task screens and App Dock

---
+++ SUITS_MagicLeap_Implemented_/Assets/Scripts/Collision/LeftCanvasCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentTaskCollider : GenericCollision
+{
+    
+    public GameObject topPanel { get; set; }
+    public GameObject mainScreen { get; set; }
+
+    protected void Start()
+    {
+        
+
+        topPanel = GameObject.FindWithTag("TopLCanvas");
+        mainScreen = GameObject.FindWithTag("MainScreenTopPanel");
+    }
+
+    public override void isOn()
+    {
+        
+        topPanel.SetActive(true);
+        mainScreen.SetActive(false);
+    }
+
+    public override void isOff()
+    {
+      
+        topPanel.SetActive(false);
+        mainScreen.SetActive(true);
+    }
+}
