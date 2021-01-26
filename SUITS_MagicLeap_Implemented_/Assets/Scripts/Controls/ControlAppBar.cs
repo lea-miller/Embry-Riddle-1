5e1d71472b1afd212ca3d9cbc22e48b88dd3b55f
MikeF98 <fornitomichael@gmail.com>
2021-01-26 02:04:08 -0500

    Icons -Backend- Move in the correct order

--- SUITS_MagicLeap_Implemented_/Assets/Scripts/Controls/ControlAppBar.cs
+++ SUITS_MagicLeap_Implemented_/Assets/Scripts/Controls/ControlAppBar.cs
@@ -6,47 +6,115 @@ using UnityEngine.UI;
 public class ControlAppBar : ControlCommands
 {
     private Image border;
-    private List <GameObject> gameObjectList;
-    private int counter = 0;
-    public bool moveFirstToLast, moveSecondToFirst, moveThirdToSecond, moveFourthToThird = false;
+    LinkedList<GameObject> gameObjectList;
+    private int triggerCount = 0;
+    public bool isTrigger = false;
     
     void Awake()
     {
         base.Awake();
         Image appImage = GameObject.FindWithTag("AppBar").GetComponent<Image>();
         border = appImage;
-        gameObjectList = new List<GameObject>();
-        gameObjectList.Add(GameObject.FindWithTag("TaskScreen"));
-        gameObjectList.Add(GameObject.FindWithTag("ScienceScreen"));
-        gameObjectList.Add(GameObject.FindWithTag("NavigationScreen"));
-        gameObjectList[0].SetActive(true);
-        gameObjectList[1].SetActive(false);
-        gameObjectList[2].SetActive(false);
+        gameObjectList = new LinkedList<GameObject>();
+        gameObjectList.AddLast(GameObject.FindWithTag("TaskScreen"));
+        gameObjectList.AddLast(GameObject.FindWithTag("ScienceScreen"));
+        gameObjectList.AddLast(GameObject.FindWithTag("NavigationScreen"));
+        gameObjectList.AddLast(GameObject.FindWithTag("MediaScreen"));
+        
+        var innerNode =  gameObjectList.First;
+        for(int i = 0; i<gameObjectList.Count-1;i++)
+        {
+            innerNode.Value.SetActive(false);
+            innerNode = innerNode.Next;
+        }
+        
+        gameObjectList.First.Value.SetActive(true);
+
+        Dictionary<int, System.Action> iconTriggerDict = new Dictionary<int, System.Action>();
+        iconTriggerDict.Add(0,MoveFirstToLast);
+        iconTriggerDict.Add(1,MoveSecondToFirst);
+        iconTriggerDict.Add(2,MoveThirdToSecond);
+        iconTriggerDict.Add(3,MoveFourthToThird);
+        
+        Dictionary<int, System.Action> iconBumperDict = new Dictionary<int, System.Action>();
+        iconBumperDict.Add(0,MoveFirstToSecond);
+        iconBumperDict.Add(1,MoveSecondToThird);
+        iconBumperDict.Add(3,MoveThirdToFourth);
+        iconBumperDict.Add(4,MoveLastToFirst);
     }
 
     public override void triggerDown()
     {   
-       if(border.enabled)
-       {
-           if(counter <= gameObjectList.Count-1)
-           {
-                gameObjectList[counter].SetActive(false);
-                counter = counter + 1;     
-                gameObjectList[counter].SetActive(true); 
-           } 
-       }
+        Debug.Log("Before");
+        display();
+        
+        //Moves the order of the lists
+        var oldFirst = gameObjectList.First.Value;
+        gameObjectList.RemoveFirst();
+        gameObjectList.AddLast(oldFirst);
+        
+        Debug.Log("After");
+        display();
+       // gameObjectList[counter].SetActive(false);
+    }
+
+    //For testing purposes
+    private void display()
+    {
+        
+        var innerNode =  gameObjectList.First;
+        for(int i = 0; i<gameObjectList.Count;i++)
+        {
+            Debug.Log(innerNode.Value);
+            innerNode = innerNode.Next;
+        }
     }
 
     public override void bumperDown()
     {
-        if(border.enabled)
-       {
-            if(counter > 0)
-           {
-                gameObjectList[counter].SetActive(false);
-                counter = counter - 1;   
-                gameObjectList[counter].SetActive(true);   
-           }
-       }
+        isTrigger = false;
+    
+
     }
+    
+    public void MoveFirstToLast()
+    {
+                                                                                                                                    
+    }
+    
+    public void MoveSecondToFirst()
+    {
+                                                                                                                                    
+    }
+    
+    public void MoveThirdToSecond()
+    {
+                                                                                                                                    
+    }
+
+    public void MoveFourthToThird()
+    {
+                                                                                                                                    
+    }
+    
+    public void MoveFirstToSecond()
+    {
+                                                                                                                                    
+    }
+    
+    public void MoveSecondToThird()
+    {
+                                                                                                                                    
+    }
+
+    public void MoveThirdToFourth()
+    {
+                                                                                                                                    
+    }
+
+    public void MoveLastToFirst()
+    {
+                                                                                                                                    
+    }
+
 }
