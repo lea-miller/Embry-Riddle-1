15fb1af3e13f02c2439c3d341a2cc790c6679344
MikeF98 <fornitomichael@gmail.com>
2021-01-26 23:37:48 -0500

    BackEnd Functions Implemented
    Fixed animator controller for but still needs to include parameters for all others but tasks. Modified the appbar to now call the system.actions which will be used to talk to the gameobjects

--- SUITS_MagicLeap_Implemented_/Assets/Scripts/Controls/ControlAppBar.cs
+++ SUITS_MagicLeap_Implemented_/Assets/Scripts/Controls/ControlAppBar.cs
@@ -7,8 +7,12 @@ public class ControlAppBar : ControlCommands
 {
     private Image border;
     LinkedList<GameObject> gameObjectList;
+    List<System.Action> iconTriggerDict;
+    List<System.Action> iconBumperDict;
     private int triggerCount = 0;
+    private GameObject tempIconAnimation; //sadly this is globalized in the code, not sure how to pass parameters in system actions
     public bool isTrigger = false;
+
     
     void Awake()
     {
@@ -30,38 +34,47 @@ public class ControlAppBar : ControlCommands
         
         gameObjectList.First.Value.SetActive(true);
 
-        Dictionary<int, System.Action> iconTriggerDict = new Dictionary<int, System.Action>();
-        iconTriggerDict.Add(0,MoveFirstToLast);
-        iconTriggerDict.Add(1,MoveSecondToFirst);
-        iconTriggerDict.Add(2,MoveThirdToSecond);
-        iconTriggerDict.Add(3,MoveFourthToThird);
+        iconTriggerDict = new List<System.Action>();
+        iconTriggerDict.Add(MoveFirstToLast);
+        iconTriggerDict.Add(MoveSecondToFirst);
+        iconTriggerDict.Add(MoveThirdToSecond);
+        iconTriggerDict.Add(MoveFourthToThird);
         
-        Dictionary<int, System.Action> iconBumperDict = new Dictionary<int, System.Action>();
-        iconBumperDict.Add(0,MoveFirstToSecond);
-        iconBumperDict.Add(1,MoveSecondToThird);
-        iconBumperDict.Add(3,MoveThirdToFourth);
-        iconBumperDict.Add(4,MoveLastToFirst);
+        iconBumperDict = new List<System.Action>();
+        iconBumperDict.Add(MoveFirstToSecond);
+        iconBumperDict.Add(MoveSecondToThird);
+        iconBumperDict.Add(MoveThirdToFourth);
+        iconBumperDict.Add(MoveLastToFirst);
     }
 
     public override void triggerDown()
     {   
-        Debug.Log("Before");
-        display();
-        
-        //Moves the order of the lists
+        moveIconAnimationForward();
+        moveIconsForward();
+    }
+
+    private void moveIconsForward()
+    {
+      //Moves the order of the lists by having the first one go to the last one
         var oldFirst = gameObjectList.First.Value;
         gameObjectList.RemoveFirst();
         gameObjectList.AddLast(oldFirst);
-        
-        Debug.Log("After");
-        display();
-       // gameObjectList[counter].SetActive(false);
+    }
+
+    private void moveIconAnimationForward()
+    {
+        var  icon = gameObjectList.First;
+        iconTriggerDict.ForEach(delegate(System.Action currAnimation)
+        {
+            tempIconAnimation = icon.Value;
+            currAnimation.Invoke();
+            icon = icon.Next;  
+        });   
     }
 
     //For testing purposes
     private void display()
-    {
-        
+    { 
         var innerNode =  gameObjectList.First;
         for(int i = 0; i<gameObjectList.Count;i++)
         {
@@ -73,28 +86,26 @@ public class ControlAppBar : ControlCommands
     public override void bumperDown()
     {
         isTrigger = false;
-    
-
     }
     
     public void MoveFirstToLast()
     {
-                                                                                                                                    
+        Debug.Log("MoveFirstToLast: " + tempIconAnimation);                                                                                                                     
     }
     
     public void MoveSecondToFirst()
     {
-                                                                                                                                    
+        Debug.Log("Move2ndTo1st" + tempIconAnimation);                                                                                        
     }
     
     public void MoveThirdToSecond()
     {
-                                                                                                                                    
+        Debug.Log("Move3rdTo2nd" + tempIconAnimation);                                                                                                                               
     }
 
     public void MoveFourthToThird()
     {
-                                                                                                                                    
+        Debug.Log("Move4thTo3rd" + tempIconAnimation);                                                                                                                    
     }
     
     public void MoveFirstToSecond()
