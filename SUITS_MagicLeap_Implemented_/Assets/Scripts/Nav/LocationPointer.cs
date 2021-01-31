233a84faca691979810b1881acbaf25c230d3faf
MikeF98 <fornitomichael@gmail.com>
2021-01-31 02:45:49 -0500

    Navigation Target Tracking P1
    Works Horizontally

--- SUITS_MagicLeap_Implemented_/Assets/Scripts/Nav/LocationPointer.cs
+++ SUITS_MagicLeap_Implemented_/Assets/Scripts/Nav/LocationPointer.cs
@@ -13,13 +13,11 @@ public class LocationPointer : MonoBehaviour
     private float rotation = 0.4f;
     private Quaternion currentScreenRotation, torsoRot;
     private GameObject torso, controller;
+    Vector3 target;
 
     private void Awake()
     {
-        // targetPosition = new Vector3(destinationX,destinationY,destinationZ);
-        // pointerTransform = transform.Find("TragetPointer").GetComponent<Transform>();
-        // controller = GameObject.FindWithTag("MLController");
-        // torso = GameObject.FindWithTag("Torso");
+        pointerTransform = this.transform;
     }
     void Start()
     {
@@ -27,12 +25,17 @@ public class LocationPointer : MonoBehaviour
     }
     private void Update()
     {
+        torso = GameObject.FindWithTag("Torso");
+        target = GameObject.Find("Target").transform.position;;
+        targetPosition = new Vector3(target.x,target.y,target.z);
+       
         rotationSync();
-        // Vector3 toTarget = targetPosition;
-        // Vector3 currPosition = transform.Find("Torso").position;
-        // Vector3 dir = (toTarget - currPosition).normalized;
-        // float angle =  (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) % 360;
-        //pointerTransform.localEulerAngles = new Vector3(currPosition.x,currPosition.y,angle);
+        
+        Vector3 toTarget = targetPosition;
+        Vector3 currPosition = torso.GetComponent<Transform>().position;
+        Vector3 dir = (toTarget - currPosition).normalized;
+        float angle =  (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) % 360;
+        pointerTransform.localEulerAngles = new Vector3(0,0,angle);
     }
 
     private void distanceFromHead()
