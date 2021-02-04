497b95179667ba759ff265cfbccd892da3a1b1d5
MikeF98 <fornitomichael@gmail.com>
2021-02-03 22:53:45 -0500

    For Dev Purposes

---
+++ SUITS_MagicLeap_Implemented_/Assets/Scripts/ModifyScreenForDevs.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifyScreenForDevs : MonoBehaviour
+{
+  
+  public GameObject TaskScreen, NavScreen, SciScreen, MediaScreen, AppBar;
+  public bool taskCondition,navCondition,sciCondition,mediaCondition;
+
+  void Start()
+  {
+    //   TaskScreen = GameObject.FindWithTag("TaskScreen");
+    //  NavScreen = GameObject.FindWithTag("NavigationScreen");
+    //   SciScreen = GameObject.FindWithTag("ScienceScreen");
+    //   MediaScreen = GameObject.FindWithTag("MediaScreen");
+    //   AppBar = GameObject.FindWithTag("AppBar");
+  }
+
+  void  FixedUpdate()
+  {
+      AppBar.SetActive(false);
+      TaskScreen.SetActive(taskCondition);
+      NavScreen.SetActive(navCondition);
+      SciScreen.SetActive(sciCondition);
+      MediaScreen.SetActive(mediaCondition);
+  }
+  
+}
