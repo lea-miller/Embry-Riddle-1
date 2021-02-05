ecd64bed1347cee97146d8199b2fc43da6c7c343
kgrussell99 <kgrussell99@users.noreply.github.com>
2021-02-05 17:58:52 -0500

    Compass & Navigation Screen Foundations
    Basic compass installed with torso and head headings, as well as degree screen. Bugs in head heading; degree screen still to be attached to head movement; compass initial heading to be added for north identification. Basic nav screen added with filler map grid image. Additional panels can easily be added below map; functionality is WOP.

---
+++ SUITS_MagicLeap_Implemented_/Assets/Compass/Scripts/HeadingController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadingController : MonoBehaviour
+{
+    public Transform headHeading;
+    public Transform torsoHeading;
+    private Quaternion torsoRot, headRot;
+    private GameObject torso, head, controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        torso = GameObject.FindWithTag("Torso");
+        head = GameObject.FindWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateTorsoHeading();
+        rotateHeadHeading();
+    }
+
+    //Torso Heading update function
+    public void rotateTorsoHeading()
+    {
+        torsoRot = torso.transform.rotation;
+        torsoHeading.transform.rotation = torsoRot;
+    }
+
+    //Head Heading update function
+    public void rotateHeadHeading()
+    {
+        headRot = torso.transform.rotation;
+        headRot.y = head.transform.rotation.y;
+        headHeading.transform.rotation = headRot;
+    }
+}
