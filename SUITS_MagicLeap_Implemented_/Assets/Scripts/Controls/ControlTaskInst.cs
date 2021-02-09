f432a58c95e60fcf738b01f29e14887ec4b4d814
MikeF98 <fornitomichael@gmail.com>
2021-02-09 04:30:06 -0500

    Added Additonal Control Logic and Finished Task's additional control logic!

--- SUITS_MagicLeap_Implemented_/Assets/Scripts/Controls/ControlTaskInst.cs
+++ SUITS_MagicLeap_Implemented_/Assets/Scripts/Controls/ControlTaskInst.cs
@@ -60,6 +60,6 @@ public class ControlTaskInst : ControlCommands
 
     public override void bumperHold()
     {
-        loadingBar.changeColorToSkip();
+        loadingBar.changeColorToIncomplete();
     }
 }
--- SUITS_MagicLeap_Implemented_/Assets/Scripts/Controls/userControl.cs
+++ SUITS_MagicLeap_Implemented_/Assets/Scripts/Controls/userControl.cs
@@ -6,34 +6,101 @@ public class userControl : MonoBehaviour
 {
     RaycastHit hit;
     private ControlCommands collide;
+    private bool isTrigger = false;
+    private bool isTriggerHold = false;
+    private int count = 0;
+    private bool isBump = false;
+    private bool isBumperHold = false;
+    private int countBump = 0;
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
         check();
+        checkTrigHold();
+        checkBumpHold();
+    }
+
+    public void checkTrigHold()
+    {
+        if(collide != null)
+        {
+            if(isTriggerHold)
+            {
+                count = count + 1;
+                if(isTrigger)
+                {
+                    count = 0;
+                    isTriggerHold = false;
+                    isTrigger = false; //have this a synchronous precaution although unlikely an out of synch error would occur
+                    collide.triggerUp();
+                    Debug.Log("Triggered");
+                }
+                else if(count >= 70)
+                {
+                    isTriggerHold = false;
+                    collide.triggerHold();
+                    Debug.Log("Trigger Held");
+                }
+            }   
+        }
     }
 
     public void triggerUp()
     {
         if(collide != null)
         {
-            collide.triggerUp();
+            if(count >= 30)
+            {
+                count = 0;
+                isTrigger = false;
+            }else
+            {
+                isTrigger = true;
+            }
+            
         }
     }
 
-    public void triggerHold()
+    public void triggerDown()
     {
         if(collide != null)
         {
-            collide.triggerHold();
+            isTriggerHold = true;
         }
     }
 
-     public void bumperHold()
+    public void checkBumpHold()
     {
         if(collide != null)
         {
-            collide.bumperHold();
+            if(isBumperHold)
+            {
+                countBump = countBump + 1;
+                if(isBump)
+                {
+                    countBump = 0;
+                    isBumperHold = false;
+                    isBump = false; //have this a synchronous precaution although unlikely an out of synch error would occur
+                    Debug.Log("Bumper");
+                    collide.bumperUp();
+                }
+                else if(countBump >= 70)
+                {
+                    isBumperHold = false;
+                    Debug.Log("Bumper Held");
+                    collide.bumperHold();
+                }
+            }   
+        }
+    }
+
+      public void bumperDown()
+    {
+        if(collide !=null)
+        {
+            isBumperHold = true;
         }
     }
 
@@ -41,7 +108,14 @@ public class userControl : MonoBehaviour
     {
         if(collide !=null)
         {
-            collide.bumperUp();
+            if(countBump >= 30)
+            {
+                countBump = 0;
+                isBump = false;
+            }else
+            {
+                isBump = true;
+            }
         }
     }
 
